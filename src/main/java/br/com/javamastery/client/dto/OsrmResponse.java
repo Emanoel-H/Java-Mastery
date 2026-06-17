52684f40ee3b0b23768d027d97e7285a5d7552e3
Emanoel <emanoel.pluto@gmail.com>
2026-06-17 17:41:21 -0300

    building class OsrmResponse

--- src/main/java/br/com/javamastery/client/dto/OsrmResponse.java
+++ src/main/java/br/com/javamastery/client/dto/OsrmResponse.java
@@ -1,5 +1,30 @@
 package br.com.javamastery.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
 public class OsrmResponse {
-    
+    private String code;
+    private List<Route> routes;
+
+    @Setter
+    @Getter
+    public static class Route {
+        private double distance;
+        private double duration;
+    }
+
+    public double getDistanceInKM(){
+        if(routes==null || routes.isEmpty())
+            throw new RuntimeException("No routes found in OSRM Response");
+
+        return routes.getFirst().getDistance() / 1000;
+    }
+
 }
