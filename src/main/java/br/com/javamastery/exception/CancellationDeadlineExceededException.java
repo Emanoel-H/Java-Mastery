c53d1af26991327ca1586fda2af605e23cbf8fa2
Emanoel <emanoel.pluto@gmail.com>
2026-06-21 10:14:31 -0300

    building CancellationDeadlineExceededException class

--- src/main/java/br/com/javamastery/exception/CancellationDeadlineExceededException.java
+++ src/main/java/br/com/javamastery/exception/CancellationDeadlineExceededException.java
@@ -2,6 +2,6 @@ package br.com.javamastery.exception;
 
 public class CancellationDeadlineExceededException extends RuntimeException {
     public CancellationDeadlineExceededException() {
-        super();
+        super("Cancellation deadline exceeded! You can only cancel a ticket up to 1 hour before its departure.");
     }
 }
