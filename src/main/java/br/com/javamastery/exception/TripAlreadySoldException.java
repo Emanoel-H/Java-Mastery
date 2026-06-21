7bc3194de8402d0dfbca636bd885345a10651790
Emanoel <emanoel.pluto@gmail.com>
2026-06-21 10:19:53 -0300

    building TripAlreadySoldException class

--- src/main/java/br/com/javamastery/exception/TripAlreadySoldException.java
+++ src/main/java/br/com/javamastery/exception/TripAlreadySoldException.java
@@ -2,6 +2,6 @@ package br.com.javamastery.exception;
 
 public class TripAlreadySoldException extends RuntimeException {
     public TripAlreadySoldException(String code) {
-        super("");
+        super("Trip "+ code +" can't be deleted for it's already related to a sale.");
     }
 }
