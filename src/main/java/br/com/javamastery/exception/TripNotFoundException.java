ff82d908c3b77208afc21981a99464c17803a01b
Emanoel <emanoel.pluto@gmail.com>
2026-06-21 09:05:55 -0300

    building exception class TripNotFoundException

--- src/main/java/br/com/javamastery/exception/TripNotFoundException.java
+++ src/main/java/br/com/javamastery/exception/TripNotFoundException.java
@@ -1,4 +1,7 @@
 package br.com.javamastery.exception;
 
 public class TripNotFoundException extends RuntimeException{
+    public TripNotFoundException(String code){
+        super("Trip not found with code " + code);
+    }
 }
