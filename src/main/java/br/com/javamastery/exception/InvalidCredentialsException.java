695d874a1b1a501d4f0d1ee5a2b494763f7b645e
Emanoel <emanoel.pluto@gmail.com>
2026-06-21 10:05:53 -0300

    building InvalidCredentialsException class

--- src/main/java/br/com/javamastery/exception/InvalidCredentialsException.java
+++ src/main/java/br/com/javamastery/exception/InvalidCredentialsException.java
@@ -1,7 +1,7 @@
 package br.com.javamastery.exception;
 
 public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);
+    public InvalidCredentialsException() {
+        super("Email or password is invalid.");
     }
 }
