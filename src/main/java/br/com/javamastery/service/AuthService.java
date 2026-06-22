48e0ef6e749244b2e539b82d4836d116e7d9ada0
Emanoel <emanoel.pluto@gmail.com>
2026-06-22 07:40:12 -0300

    building AuthService class

--- src/main/java/br/com/javamastery/service/AuthService.java
+++ src/main/java/br/com/javamastery/service/AuthService.java
@@ -1,5 +1,35 @@
 package br.com.javamastery.service;
 
+import br.com.javamastery.dao.EmailDAO;
+import br.com.javamastery.exception.EmailAlreadyExistsException;
+import br.com.javamastery.exception.InvalidCredentialsException;
+import br.com.javamastery.models.Email;
+import jakarta.persistence.EntityManager;
+
 public class AuthService {
-    
+    private EmailDAO emailDAO;
+
+    public AuthService(EntityManager em) {
+        this.emailDAO = new EmailDAO(em);
+    }
+
+    public Email login(String email, String password) {
+        Email emailA = new Email();
+        emailA.setEmail(email);
+        emailA.setPassword(password);
+
+        if (!emailDAO.emailExists(emailA))
+            throw new InvalidCredentialsException();
+
+        return emailA;
+    }
+
+    public void checkEmailAvailable(String email) {
+        Email emailA = new Email();
+        emailA.setEmail(email);
+
+        if (emailDAO.emailExists(emailA))
+            throw new EmailAlreadyExistsException(email);
+    }
+
 }
