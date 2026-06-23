17f35dc474a61b80f327697259547a90d0fd73d7
Emanoel <emanoel.pluto@gmail.com>
2026-06-23 11:31:29 -0300

    creating createTrip() in TripService class

--- src/main/java/br/com/javamastery/service/TripService.java
+++ src/main/java/br/com/javamastery/service/TripService.java
@@ -2,9 +2,14 @@ package br.com.javamastery.service;
 
 import br.com.javamastery.client.OsrmClient;
 import br.com.javamastery.dao.TripDAO;
+import br.com.javamastery.exception.InvalidPriceException;
+import br.com.javamastery.models.BusCompany;
 import br.com.javamastery.models.City;
 import br.com.javamastery.models.Trip;
 import jakarta.persistence.EntityManager;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
 
 public class TripService {
     public static final double PRICE_PER_KM = 0.35;
@@ -26,5 +31,29 @@ public class TripService {
         trip.calculateRealDistance(osrmClient);
 
         return trip.getDistanceKM() * PRICE_PER_KM;
+    }
+
+    public Trip createTrip(City origin, City destination, BigDecimal price, LocalTime departureTime, BusCompany busCompany) {
+        Trip trip = new Trip();
+        trip.setOriginCity(origin);
+        trip.setDestinationCity(destination);
+
+        if (price.compareTo(BigDecimal.ZERO) > 0)
+            trip.setPrice(price);
+        else
+            throw new InvalidPriceException(price);
+
+        trip.setDepartureTime(departureTime);
+        trip.setBusCompany(busCompany);
+        trip.calculateRealDistance(osrmClient);
+
+        try {
+            this.em.getTransaction().begin();
+            this.tripDAO.save(trip);
+            this.em.getTransaction().commit();
+        } catch (Exception e) {
+            this.em.getTransaction().rollback();
+        }
+            return trip;
     }
 }
