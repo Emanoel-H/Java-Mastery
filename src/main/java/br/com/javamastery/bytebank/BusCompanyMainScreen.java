545e75f0a059a2ab3f27fd7e7f71b8052bc056b4
Emanoel <emanoel.pluto@gmail.com>
2026-06-23 11:50:03 -0300

    creating collectOriginCity() at BusCompany interface

--- src/main/java/br/com/javamastery/bytebank/BusCompanyMainScreen.java
+++ src/main/java/br/com/javamastery/bytebank/BusCompanyMainScreen.java
@@ -635,4 +635,30 @@ public class BusCompanyMainScreen {
         busCompanyDAO.save(busCompany);
         em.getTransaction().commit();
     }
+
+    private static City collectOriginCity(AddressDAO addressDAO, Scanner sc){
+        String cityName = "";
+        String stateName = "";
+        City originCity = null;
+        City cityA = new City();
+
+        while (cityName.isEmpty()) {
+            System.out.println("Type in the name of the origin city: \nIf you wish to view the cities before type 1");
+            cityName = sc.nextLine();
+
+            if (cityName.trim().charAt(0) == '1')
+                originCity = viewCities(stateName, addressDAO, sc, cityName);
+            else {
+                cityA.setCity(cityName.toLowerCase());
+                originCity = addressDAO.searchCity(cityA);
+
+                if (originCity == null){
+                    cityName = "";
+                    System.out.println("Type in a valid value!");
+                }
+            }
+        }
+
+        return originCity;
+    }
 }
