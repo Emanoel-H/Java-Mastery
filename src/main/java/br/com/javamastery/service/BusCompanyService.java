ec3651e330c3ef7ce3ad5213bfa363101419952c
Emanoel <emanoel.pluto@gmail.com>
2026-06-25 17:07:37 -0300

    building BusCompanyService class

--- src/main/java/br/com/javamastery/service/BusCompanyService.java
+++ src/main/java/br/com/javamastery/service/BusCompanyService.java
@@ -1,4 +1,38 @@
 package br.com.javamastery.service;
 
+import br.com.javamastery.dao.BusCompanyDAO;
+import br.com.javamastery.models.BusCompany;
+import jakarta.persistence.EntityManager;
+
+import java.util.Objects;
+
 public class BusCompanyService {
+    EntityManager em;
+    BusCompanyDAO  busCompanyDAO;
+
+    public BusCompanyService(EntityManager em) {
+        this.em = em;
+        busCompanyDAO = new BusCompanyDAO(em);
+    }
+
+    public BusCompany signUp(String legalName, String tradingName, String cnpj, String telephone, String emailAddress, String password){
+        BusCompany busCompany = new BusCompany();
+        busCompany.setLegalName(Objects.requireNonNull(legalName, "Cannot be empty"));
+        busCompany.setTradingName(Objects.requireNonNull(tradingName, "Cannot be empty"));
+        busCompany.setCnpj(cnpj);
+        busCompany.setTelephone(telephone);
+        busCompany.getEmail().setEmail(Objects.requireNonNull(emailAddress, "Cannot be empty"));
+        busCompany.getEmail().setPassword(Objects.requireNonNull(password, "Cannot be empty"));
+
+        try{
+            this.em.getTransaction().begin();
+            this.busCompanyDAO.save(busCompany);
+            this.em.getTransaction().commit();
+        }catch(Exception e){
+            this.em.getTransaction().rollback();
+            throw e;
+        }
+
+        return busCompany;
+    }
 }
