aedaecb3d5c3974ddd86872d5882dd2dfbe0b2c5
Emanoel <emanoel.pluto@gmail.com>
2026-06-25 16:37:38 -0300

    building AddressService class

--- src/main/java/br/com/javamastery/service/AddressService.java
+++ src/main/java/br/com/javamastery/service/AddressService.java
@@ -1,4 +1,28 @@
 package br.com.javamastery.service;
 
+import br.com.javamastery.dao.AddressDAO;
+import br.com.javamastery.models.City;
+import br.com.javamastery.models.State;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
 public class AddressService {
+    AddressDAO addressDAO;
+
+    public AddressService(EntityManager em) {
+        this.addressDAO = new AddressDAO(em);
+    }
+
+    public City searchCity(City city){
+        return addressDAO.searchCity(city);
+    }
+
+    public List<State> searchAllState(){
+        return addressDAO.searchAllState();
+    }
+
+    public List<City> searchCitiesByState(String stateName){
+        return addressDAO.searchCitiesByState(stateName);
+    }
 }
