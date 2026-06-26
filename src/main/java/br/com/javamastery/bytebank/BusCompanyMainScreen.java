faa30b0b51599f4daf9c6bddc70284160d0c5755
Emanoel <emanoel.pluto@gmail.com>
2026-06-26 11:07:04 -0300

    removing unused variables from deleteTrip() at BusCompany interface

--- src/main/java/br/com/javamastery/bytebank/BusCompanyMainScreen.java
+++ src/main/java/br/com/javamastery/bytebank/BusCompanyMainScreen.java
@@ -187,13 +187,13 @@ public class BusCompanyMainScreen {
 
             if (!allTrips.isEmpty()) {
                 editTrip(tripA, tripService, addressService);
-                deleteTrip(em, tripA, tripDAO, tripService);
+                deleteTrip(tripA, tripService);
             }
         }else
             System.out.println("There is no trips in the database yet!");
     }
 
-    private static void deleteTrip(EntityManager em, Trip tripA, TripDAO tripDAO, TripService tripService) {
+    private static void deleteTrip(Trip tripA, TripService tripService) {
         Scanner sc = new Scanner(System.in);
         System.out.println("""
                 Do you wish to delete a trip?
